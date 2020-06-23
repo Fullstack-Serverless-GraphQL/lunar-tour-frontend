56b88e9b0ce18503ef0574d6187416ef83702a55
AmoDinho <u14284783@tuks.co.za>
2020-06-23 19:16:27 +0200

    created removebutton

---
+++ src/components/buttons/RemoveButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    @click="handleClick"
+    class="bg-white cursor-pointer text-red-darkest focus:outline-none font-display  px-16 py-2 cursor:pointer"
+  >
+    <div class="flex flex-row">
+      <img src="../../assets/remove.svg" />
+      <p class="text-sm font-display text-bold ml-3 ">Remove</p>
+    </div>
+  </button>
+</template>
+<script>
+export default {
+  name: "RemoveButton",
+  methods: {
+    handleClick(evt) {
+      this.$emit("click", evt);
+    },
+  },
+};
+</script>
