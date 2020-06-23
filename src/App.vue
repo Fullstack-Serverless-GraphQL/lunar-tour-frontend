2387f1d00ac7ca7950e47520422c4ac8b73aba48
AmoDinho <u14284783@tuks.co.za>
2020-06-23 19:55:17 +0200

    added navs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
+    <Nav />
     <router-view />
   </div>
 </template>
+<script>
+import Nav from "./components/navs/Nav.vue";
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "App",
+  components: {
+    Nav,
+  },
+};
+</script>
