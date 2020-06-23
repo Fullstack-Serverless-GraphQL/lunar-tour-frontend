bbaa6a2251f554e6dd31b17588feaa139ab8c6d8
AmoDinho <u14284783@tuks.co.za>
2020-06-23 19:13:30 +0200

    created imput component

---
+++ src/components/inputs/Input.vue
@@ -0,0 +1,34 @@
+<template>
+  <input
+    class="
+   bg-white 
+    rounded-px 
+    py-3
+    px-16 
+    border
+    border-grey-light 
+    focus:outline-none 
+    focus:bg-white"
+    type="text"
+    :placeholder="placeholder"
+    @input="handleInput"
+  />
+</template>
+<script>
+export default {
+  name: "Input",
+  props: {
+    placeholder: String,
+  },
+  data() {
+    return {
+      value: "",
+    };
+  },
+  methods: {
+    handleInput(e) {
+      this.$emit("input", e.target.value);
+    },
+  },
+};
+</script>
