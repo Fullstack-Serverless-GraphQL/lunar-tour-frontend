1310cbf0e6653ce08af009d1cbe6000ef0725a10
AmoDinho <u14284783@tuks.co.za>
2020-06-23 20:24:36 +0200

    added rate component

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/tailwind.css";
+import { Rate } from "ant-design-vue";
 import { createProvider } from "./vue-apollo";
 Vue.config.productionTip = false;
 
+Vue.use(Rate);
 new Vue({
   router,
   apolloProvider: createProvider(),
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
