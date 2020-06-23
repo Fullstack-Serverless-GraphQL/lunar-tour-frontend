1cc8317a62c136b84cc5c529aeec799cb7b86af7
AmoDinho <u14284783@tuks.co.za>
2020-06-23 19:10:51 +0200

    created read outline button

---
+++ src/components/buttons/RedOutlineButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <button
+    @click="handleClick"
+    class="cursor-pointer bg-white border 
+    border-red-light text-red-darkest focus:outline-none 
+    font-display rounded-px px-16 py-2 hover:bg-transparent 
+    hover:border hover:border-red-darkest"
+  >
+    <div v-if="isLoading">
+      <img
+        aria-label="loading-spinner"
+        class="spinner"
+        src="../../assets/loop.svg"
+      />
+    </div>
+    <div v-else>{{ text }}</div>
+  </button>
+</template>
+<script>
+export default {
+  name: "RedOutlineButton",
+  props: {
+    text: String,
+    isLoading: Boolean,
+  },
+  methods: {
+    handleClick(evt) {
+      this.$emit("click", evt);
+    },
+  },
+};
+</script>
+<style>
+.spinner {
+  display: inline-block;
+  animation: rotate 2s infinite linear;
+}
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
