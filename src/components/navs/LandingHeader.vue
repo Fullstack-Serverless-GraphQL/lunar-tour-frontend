eb73942b94b4352697e9d59f5629f92f7d3f47e9
AmoDinho <u14284783@tuks.co.za>
2020-06-23 19:15:52 +0200

    created landing header

---
+++ src/components/navs/LandingHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="image text-center bg-transparent " :style="urlProps">
+    <h1 class="text-white font-display text-5xl pt-44 s:text-4xl">
+      <div v-if="text">
+        {{ text }}
+      </div>
+      <div v-else></div>
+    </h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Landing Header",
+  props: {
+    imgURL: String,
+    text: String,
+    margin: String,
+  },
+  computed: {
+    urlProps() {
+      return "--img-url:" + `url(${this.imgURL})`;
+    },
+  },
+};
+</script>
+<style>
+.image {
+  background: var(--img-url) no-repeat center center;
+  background-size: cover;
+  height: 570px;
+  margin-top: -85px;
+}
+</style>
