6cce02555040c8f73c5220cd5b90e62f3079f029
AmoDinho <u14284783@tuks.co.za>
2020-06-23 19:17:22 +0200

    created heading one

---
+++ src/components/typography/HeadingOne.vue
@@ -0,0 +1,13 @@
+<template>
+  <p class="text-4xl text-black text-bold font-display">
+    <slot> </slot>
+  </p>
+</template>
+<script>
+export default {
+  name: "HeadingOne",
+  props: {
+    text: String,
+  },
+};
+</script>
