5452610097b8c245c8cae0770118a434f067a1bf
AmoDinho <u14284783@tuks.co.za>
2020-06-24 19:44:10 +0200

    created customers tab file

--- src/views/booking/BookingIndex.vue
+++ src/views/booking/BookingIndex.vue
@@ -1,71 +1,52 @@
 <template>
   <div>
+    <a-tabs v-model="activeKey">
+      <a-tab-pane tab="Tab 1" key="1" class="flex justify-center">
+        <p>tab1</p>
+      </a-tab-pane>
 
-        <a-tabs v-model="activeKey">
-      <p>tab1</p>
-          </a-tab-pane>
+      <!-- TAB ONE  END--->
 
-          <!-- TAB ONE  END--->
+      <!-- TAB TWO --->
 
-          <!-- TAB TWO --->
+      <a-tab-pane tab="Tab 2" key="2" class="flex justify-center">
+        <p>tab2</p>
+      </a-tab-pane>
+      <!-- TAB TWO  END--->
+      <!-- TAB THREE --->
 
-          <a-tab-pane tab="Tab 2" key="2" class="flex justify-center">
-             <p>tab2</p>
+      <a-tab-pane tab="Tab 3" key="3" class="text-black flex justify-center">
+        <p>tab3</p>
+      </a-tab-pane>
+      <!-- TAB THREE END--->
+      <!-- TAB FOUR --->
 
-          </a-tab-pane>
-          <!-- TAB TWO  END--->
-          <!-- TAB THREE --->
-
-          <a-tab-pane
-            tab="Tab 3"
-            key="3"
-            class="text-black flex justify-center"
-          >
-               <p>tab3</p>
-          </a-tab-pane>
-          <!-- TAB THREE END--->
-          <!-- TAB FOUR --->
-
-          <a-tab-pane
-            tab="Tab 4"
-            key="4"
-            class="text-black flex justify-center"
-          >
-               <p>tab4</p>
-          </a-tab-pane>
-          <!-- TAB FOUR END--->
-        </a-tabs>
+      <a-tab-pane tab="Tab 4" key="4" class="text-black flex justify-center">
+        <p>tab4</p>
+      </a-tab-pane>
+      <!-- TAB FOUR END--->
+    </a-tabs>
   </div>
 </template>
 
 <script>
-
-
-
 export default {
   name: "BookingIndex",
-  components: {
-   
-
-  },
+  components: {},
   data() {
     return {
       activeKey: "1",
-   
     };
   },
   methods: {
     next(k) {
       this.activeKey = k.toString();
-    }
-  }
-   
+    },
+  },
 };
 </script>
 <style>
 .ant-tabs-bar.ant-tabs-top-bar {
   display: none;
 }
-
-
 </style>
---
+++ src/views/booking/CustomersTab.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex flex-col p-20 ">
+    <HeadingOne>
+      Booking for Listing Name
+    </HeadingOne>
+    <div class="mt-5">
+      <BodyOne>
+        Booking date
+      </BodyOne>
+      <DateInput placeholder="date" type="date" v-model="date" />
+    </div>
+    <div class="mt-5">
+      <BodyOne>
+        Number of people
+      </BodyOne>
+      <Input placeholder="3" type="number" v-model="number" />
+    </div>
+    <div class="mt-5">
+      <BodyOne> Email address </BodyOne>
+      <Input placeholder="doku@corrisant.io" type="email" v-model="email" />
+    </div>
+    <div class="flex lg:flex-row mt-5 s:flex-col">
+      <RedBlockButton
+        @click="next(2)"
+        text="Proceed"
+        class="mr-5 s:mb-5 lg:mb-0"
+      />
+      <RedOutlineButton @click="$router.push('/')" text="Cancel" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CustomersTab",
+};
+</script>
