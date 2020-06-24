ed349266c28578495f3c660e8b7e488d0b74e8ba
AmoDinho <u14284783@tuks.co.za>
2020-06-24 19:15:51 +0200

    added listing to route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Listing from "./pages/Listing";
 Vue.use(VueRouter);
 
 const routes = [
@@ -8,6 +9,10 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/listing/:id",
+    component: Listing,
   },
 ];
 
