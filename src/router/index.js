785be25c50139fe8b42f8172d72fb5b6daef49d4
AmoDinho <u14284783@tuks.co.za>
2020-06-24 19:26:38 +0200

    thing is worrkingg

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Listing from "../views/Listing";
+import Listing from "../views/Listing.vue";
 Vue.use(VueRouter);
 
 const routes = [
