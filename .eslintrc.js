38e56c72a5b604d395e215c660b39a71a7c4ab02
AmoDinho <u14284783@tuks.co.za>
2020-08-06 20:52:58 +0200

    fixed eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,20 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "babel-eslint",
   },
   rules: {
+    "prettier/prettier": [
+      "warn",
+      {
+        trailingComma: "none",
+      },
+    ],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
 };
