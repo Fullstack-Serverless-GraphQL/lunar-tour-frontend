a023a5a206b7b7856ea46781126420c9ea2f8457
AmoDinho <u14284783@tuks.co.za>
2020-08-06 19:58:21 +0200

    created heading three

---
+++ src/components/typography/HeadingThree.vue
@@ -0,0 +1,13 @@
+<template>
+  <p class="text-xl text-black text-bold font-display">
+    <slot> </slot>
+  </p>
+</template>
+<script>
+export default {
+  name: "HeadingThree",
+  props: {
+    text: String,
+  },
+};
+</script>
