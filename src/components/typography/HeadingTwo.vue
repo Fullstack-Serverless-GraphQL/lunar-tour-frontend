2ded19b66491e655908c67e7230ff22436d154b7
AmoDinho <u14284783@tuks.co.za>
2020-08-06 19:57:53 +0200

    created HeadingTwo

---
+++ src/components/typography/HeadingTwo.vue
@@ -0,0 +1,13 @@
+<template>
+  <p class="text-2xl text-black text-bold font-display">
+    <slot> </slot>
+  </p>
+</template>
+<script>
+export default {
+  name: "HeadingTwo",
+  props: {
+    text: String,
+  },
+};
+</script>
